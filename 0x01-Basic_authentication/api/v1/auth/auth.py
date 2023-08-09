4cddf785dcf9dcea042d812365d6351e003f8c52
Kelvin <edithmukoro3@gmail.com>
2023-08-09 03:57:35 +0100

    task 04

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python3
 """ Creating a class to manage the API authentication
 """
-
-from flask import Flask, request
+from typing import (
+    List,
+    TypeVar
+)
+from flask import request
 from models.user import User
 
 
 class Auth:
+    """ API authentication class
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns False - path
         """
-        return False
+        if path is None or not excluded_paths:
+            return True
+        for i in excluded_paths:
+            if i.endswith('*') and path.startswith(i[:-1]):
+                return False
+            elif i in {path, path + '/'}:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ returns None - path
         """
-        return None
+        if request is None or "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ returns None - request
         """
         return None
-
