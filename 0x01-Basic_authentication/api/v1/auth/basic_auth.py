aa9a0073789a6a1b606f0b9c480109e8922d9c06
Kelvin <edithmukoro3@gmail.com>
2023-08-09 10:15:56 +0100

    task 07

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,11 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic Authentication that inherits from authentication
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ Extract the authorization header
+        """
+        if authorization_header isNone or type(authorization_header) != str \
+                or not authorization_header.startswith('Basic '):
+            return None
+        return ''.join(authorization_header.split('Basic ')[1:])
