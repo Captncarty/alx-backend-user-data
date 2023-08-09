bc77e6b3ec3e85c69a384bbd326f4dd77deb3918
Kelvin <edithmukoro3@gmail.com>
2023-08-09 10:31:00 +0100

    task 07 fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,10 +9,14 @@ class BasicAuth(Auth):
     """Basic Authentication that inherits from authentication
     """
     def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
+        self, authorization_header: str
+    ) -> str:
         """ Extract the authorization header
         """
-        if authorization_header isNone or type(authorization_header) != str \
-                or not authorization_header.startswith('Basic '):
+        if (
+            authorization_header is None
+            or type(authorization_header) != str
+            or not authorization_header.startswith('Basic ')
+        ):
             return None
         return ''.join(authorization_header.split('Basic ')[1:])
