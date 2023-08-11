5da13047ec09e9f91a1d938e2005e6a7d487e87f
Kelvin <edithmukoro3@gmail.com>
2023-08-11 05:33:31 +0100

    task 08

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -37,3 +37,18 @@ def session_login() -> str:
     response = jsonify(user.to_json())
     response.set_cookie(session_cookie, session_id)
     return response
+
+
+@app_views.route(
+        '/auth_session/logout',
+        methods=['DELETE'],
+        strict_slashes=False)
+def session_logout() -> str:
+    """ deletes the user session/logout
+    """
+    from api.v1.app import auth
+
+    logout = auth.destroy_session(request)
+    if not logout:
+        abort(404)
+    return jsonify({}), 200
