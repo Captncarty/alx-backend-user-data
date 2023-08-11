6aad9bd91b4f9f013749981002dc7ad20ef5d0d1
Kelvin <edithmukoro3@gmail.com>
2023-08-11 05:40:04 +0100

    task 09

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -28,6 +28,17 @@ elif auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+elif getenv("AUTH_TYPE") == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+
+elif getenv("AUTH_TYPE") == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
+
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 @app.before_request
 def request_filter() -> str:
