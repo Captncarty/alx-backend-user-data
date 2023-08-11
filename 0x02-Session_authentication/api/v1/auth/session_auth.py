4ca11811f37fbd8c1311843a295c0b50b5a9365e
Kelvin <edithmukoro3@gmail.com>
2023-08-11 03:40:30 +0100

    task 02

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,7 @@
 """
 Defines a BasicAuth class that inherits from Auth class
 """
-import base64
-from typing import Tuple, TypeVar, Union
+import uuid
 from api.v1.auth.auth import Auth
 from api.v1.views.users import User
 
@@ -11,4 +10,18 @@ from api.v1.views.users import User
 class SessionAuth(Auth):
     """ Authenticates User Session
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a Session ID
+        """
+        if (
+            user_id is None
+            or isinstance(user_id, str) is False
+        ):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
