028a8d4d5c3334d7f2569a65c796bfcf6c5f5f95
Kelvin <edithmukoro3@gmail.com>
2023-08-17 04:45:26 +0100

    task 08

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,14 +5,16 @@ Create a Flask app that has a single GET route ("/")
 and use flask.jsonify to return a JSON payload of the form:
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+auth = Auth()
 
 
 @app.route('/', methods=['GET'])
-def route():
+def route() -> str:
     """
     returns a JSON payload of the form
     """
@@ -21,6 +23,28 @@ def route():
     }
 
     return jsonify(form)
+
+
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """
+    implement the end-point to register a user
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        new_user = auth.register_user(email, password)
+        if new_user is not None:
+            return jsonify({
+                "email": new_user.email,
+                "message": "user created"
+                })
+    except ValueError:
+        return jsonify({
+            "message": "email already registered"
+            }), 400
+    return None
 
 
 if __name__ == '__main__':
