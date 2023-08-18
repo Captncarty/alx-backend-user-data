f1ebb2c1d8de516b2995e81e45b9e091a5882e38
Kelvin Maccarthy <edithmukoro3@gmail.com>
2023-08-18 05:18:09 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,3 +68,111 @@ class Auth:
             return check
         except NoResultFound:
             return False
+
+
+def create_session(self, email: str) -> str:
+        """
+        Implement the Auth.create_session method
+        takes an email string argument and returns the session ID as a string
+        The method should find the user corresponding to the email
+        generate a new UUID and store it in the database
+        as the user’s session_id, then return the session ID.
+        Remember that only public methods of self._db can be used.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union(str, None):
+        """
+        In this task, you will implement the Auth.get_user_from_session_id method.
+        It takes a single session_id string argument
+        and returns the corresponding User or None.
+        If the session ID is None or no user is found,
+        return None. Otherwise return the corresponding user.
+        Remember to only use public methods of self._db.
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None::
+        """
+        In this task, you will implement Auth.destroy_session.
+        The method takes a single user_id integer argument and returns None.
+        The method updates the corresponding user’s session ID to None.
+        Remember to only use public methods of self._db.
+        """
+        if user_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(id=user_id)
+            return self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Finds user by email, updates user's reset_toke with UUID """
+        try:
+            found_user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = _generate_uuid()
+        self._db.update_user(found_user.id, reset_token=reset_token)
+        return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Use the reset_token to find the corresponding user.
+            If it does not exist, raise a ValueError exception.
+        """
+        if reset_token is None or password is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_password,
+                             reset_token=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Finds user by email, updates user's reset_toke with UUID """
+        try:
+            found_user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = _generate_uuid()
+        self._db.update_user(found_user.id, reset_token=reset_token)
+        return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Use the reset_token to find the corresponding user.
+            If it does not exist, raise a ValueError exception.
+        """
+        if reset_token is None or password is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_password,
+                             reset_token=None)
